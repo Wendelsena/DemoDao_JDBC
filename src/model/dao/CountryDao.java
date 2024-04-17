da2e7202296aaedf8e87a434326338551937dd20
Wendelsena <wendel4019@gmail.com>
2024-04-17 16:33:22 -0300

    CategoryDao and CountryDao interfaces

---
+++ src/model/dao/CountryDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Country;
+
+public interface CountryDao {
+	
+	void insert(Country obj);
+	void update(Country obj);
+	void deleteById(Integer id);
+	Country findById(Integer id);
+	List<Country> findAll();
+}
